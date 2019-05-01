f58e63cd60ea69a7992141e62a24599227f97dcb
Santino <ha.agharbi@gmail.com>
2019-04-30 17:41:19 -0700

    added routes for the new front pages

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,15 @@ const path = require("path");
 const router = require("express").Router();
 
 router.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../views/assets/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-router.get("/login", function(req, res) {
-  res.sendFile(path.join(__dirname, "../views/assets/html/login.html"));
+router.get("/signin", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/signin.html"));
 });
 
-router.get("/login2", function(req, res) {
-  res.sendFile(path.join(__dirname, "../views/assets/html/login2.html"));
-});
-
-router.get("/mock-dashboard", function(req, res) {
-  res.sendFile(path.join(__dirname, "../views/assets/html/dashboard.html"));
-});
-
-router.get("/invoice", function(req, res) {
-  res.sendFile(path.join(__dirname, "../views/assets/html/invoice.html"));
+router.get("/signup", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/signup.html"));
 });
 
 // If no other routes are hit, send the React app
