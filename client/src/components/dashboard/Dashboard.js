b8d91d87d43bdc46f543f3d87a67482ea8188965
Dendevpro <mauro.facciolla@gmail.com>
2019-05-12 00:15:23 -0700

    Initial Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import { findUserDetails } from "../../actions/findUserActions";
+import MiniDrawer from "../MiniDrawer/MiniDrawer";
 
 class Dashboard extends Component {
 
@@ -17,10 +18,10 @@ class Dashboard extends Component {
     const userData = {
       id: this.props.auth.user.id
     };
-    console.log('componentdid',this.props.auth.user.id)
+    console.log('componentdid', this.props.auth.user.id)
     this.props.findUserDetails(userData);
   }
-  
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.errors) {
       this.setState({
@@ -39,31 +40,34 @@ class Dashboard extends Component {
     const { data } = this.props.userDetails;
     console.log(user);
     console.log(data)
-  return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.firstName} 
-              <p className="flow-text grey-text text-darken-1">
-                <span> You made it to SUMIT {""} 👏 </span>
-              </p>
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
-          </div>
-        </div>
-      </div>
+    return (
+      <div>
+        <MiniDrawer />
+      </div >
+      // <div style={{ height: "75vh" }} className="container valign-wrapper">
+      //   <div className="row">
+      //     <div className="col s12 center-align">
+      //       <h4>
+      //         <b>Hey there,</b> {user.firstName} 
+      //         <p className="flow-text grey-text text-darken-1">
+      //           <span> You made it to SUMIT {""} 👏 </span>
+      //         </p>
+      //       </h4>
+      //       <button
+      //         style={{
+      //           width: "150px",
+      //           borderRadius: "3px",
+      //           letterSpacing: "1.5px",
+      //           marginTop: "1rem"
+      //         }}
+      //         onClick={this.onLogoutClick}
+      //         className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+      //       >
+      //         Logout
+      //       </button>
+      //     </div>
+      //   </div>
+      // </div>
     );
   }
 }
