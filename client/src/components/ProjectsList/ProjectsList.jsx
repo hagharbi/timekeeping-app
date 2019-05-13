750164bfabf3a42997e01e17b00e3e36226e3990
Dendevpro <mauro.facciolla@gmail.com>
2019-05-13 16:26:45 -0700

    Projects Page

---
+++ client/src/components/ProjectsList/ProjectsList.jsx
@@ -0,0 +1,158 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import Fab from '@material-ui/core/Fab';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+
+const styles = theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        flexShrink: 0,
+        color: theme.palette.text.secondary,
+        marginLeft: theme.spacing.unit * 2.5,
+    },
+    dense: {
+        marginTop: 19,
+    },
+    menu: {
+        width: 200,
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        Width: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing.unit * 2,
+    },
+
+});
+
+// Set Priority color to change according to the user selection
+let priorityColor = colors => ({
+
+})
+
+// Set Status field color to change according to the user selection
+let statusColor = colors => ({
+
+})
+
+class TextFields extends React.Component {
+    state = {
+        project: '',
+        client: '',
+        category: '',
+        status: '',
+        priority: '',
+    };
+
+    handleChange = name => event => {
+        this.setState({ [name]: event.target.value });
+    };
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <form
+                style={{
+                    margin: "1rem auto 0 auto",
+                }}>
+
+                <TextField
+                    required
+                    id="standard-required"
+                    label="Title"
+                    className={classes.textField}
+                    value={this.state.name}
+                    onChange={this.handleChange('name')}
+                    margin="normal"
+                />
+
+                <TextField
+                    required
+                    id="standard-required"
+                    label="Client"
+                    defaultValue=""
+                    className={classes.textField}
+                    margin="normal"
+                />
+
+                <TextField
+                    required
+                    id="standard-required"
+                    label="Category"
+                    defaultValue=""
+                    className={classes.textField}
+                    margin="normal"
+                />
+                <FormControl className={classes.formControl}
+                    style={{
+                        margin: "1rem auto 0 2rem",
+                    }}>
+                    <InputLabel htmlFor="age-native-simple">Status</InputLabel>
+                    <Select
+                        native
+                        value={this.state.status}
+                        onChange={this.handleChange('status')}
+                        inputProps={{
+                            name: 'status',
+                            id: '',
+                        }}
+                    >
+                        <option value="" />
+                        <option>Active</option>
+                        <option>Inactive</option>
+                        <option>On Hold</option>
+                    </Select>
+                </FormControl>
+
+                <FormControl className={classes.formControl}
+                    style={{
+                        margin: "1rem auto 0 2rem",
+                    }}>
+                    <InputLabel htmlFor="priority-native-simple">Priority</InputLabel>
+                    <Select
+                        native
+                        value={this.state.priority}
+                        onChange={this.handleChange('priority')}
+                        inputProps={{
+                            name: 'priority',
+                            id: '',
+                        }}
+                    >
+                        <option value="" />
+                        <option>High</option>
+                        <option>Medium</option>
+                        <option>Low</option>
+                    </Select>
+                </FormControl>
+
+                <Fab size="small" color="secondary" aria-label="Play" className={classes.margin}
+                    style={{
+                        margin: "3rem auto 0 1rem",
+                        backgroundColor: "#999",
+                    }}
+                >
+                    <PlayArrowIcon />
+                </Fab>
+
+            </form>
+        );
+    }
+}
+
+TextFields.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(TextFields);
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -4,9 +4,10 @@ import { connect } from "react-redux";
 import { logoutUser } from "../actions/authActions";
 import { findUserDetails } from "../actions/findUserActions";
 import ResponsiveDrawer from "../components/ResponsiveDrawer/ResponsiveDrawer";
+import ProjectsList from "../components/ProjectsList/ProjectsList";
 import Divider from '@material-ui/core/Divider';
 
-class Dashboard extends Component {
+class Projects extends Component {
 
     constructor() {
         super();
@@ -43,47 +44,14 @@ class Dashboard extends Component {
         // console.log(data)
         return (
             <>
-                <div>
+                <div style={{
+                    margin: "0 auto 0 2rem",
+                }}>
                     <ResponsiveDrawer />
                     <div className="container">
-                        <h1>DASHBOARD</h1>
-                        <div className="row">
-                            <div className="col s6">
-                                <div className="card">
-                                    <div className="card-header">
-                                        <h6>
-                                            Recent Acitivity
-                    </h6>
-                                    </div>
-                                    <Divider />
-                                    <div className="card-content">
-                                        <ul>
-                                            <li>
-                                                Project 1
-                      </li>
-                                            <li>
-                                                Project 2
-                      </li>
-                                            <li>
-                                                Project 3
-                      </li>
-                                            <li>
-                                                Project 4
-                      </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col s6">
-                                <div className="card">
-                                    <div className="card-header">
-                                        <h6>
-                                            Week Earnings
-                  </h6>
-                                        <Divider />
-                                    </div>
-                                </div>
-                            </div>
+                        <h1>PROJECTS</h1>
+                        <div>
+                            <ProjectsList />
                         </div>
                     </div>
                 </div >
@@ -92,7 +60,7 @@ class Dashboard extends Component {
     }
 }
 
-Dashboard.propTypes = {
+Projects.propTypes = {
     findUserDetails: PropTypes.func.isRequired,
     logoutUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
@@ -113,4 +81,4 @@ const mapStateToProps = state => ({
 export default connect(
     mapStateToProps,
     { logoutUser, findUserDetails }
-)(Dashboard);
+)(Projects);
