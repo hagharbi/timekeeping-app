95cb1df066c1c97b8132ccb19112666ac315fa64
Kara Nichols <lnicholskara@gmail.com>
2019-05-14 07:37:23 -0700

    Update clients.js

--- routes/api/clients.js
+++ routes/api/clients.js
@@ -5,7 +5,7 @@ const clientsController = require("../../controllers/clients.js");
 //***** FIND ***** */
 
 router.route("/finduser")
-      .post(clientsController.findOneUser);
+    .post(clientsController.findOneUser);
 
 router.route("/findclient")
     .post(clientsController.findOneClient);
@@ -19,15 +19,15 @@ router.route("/updateclient")
     .post(clientsController.updateClient);
 
 router.route("/updateuser")
-      .post(clientsController.updateUser);
+    .post(clientsController.updateUser);
 
 router.route("/updatenotes")
-        .post(clientsController.addNotes);
+    .post(clientsController.addNotes);
 
 router.route("/removeclient")
     .post(clientsController.removeClient);
 
 router.route("/removeuser")
-      .post(clientsController.removeUser);
+    .post(clientsController.removeUser);
 
 module.exports = router;
