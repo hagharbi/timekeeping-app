1cd363b806facfdc09ee1d458a1be668c1f83568
Kara Nichols <lnicholskara@gmail.com>
2019-05-14 12:43:41 -0700

    edited code to have invoice page render

---
+++ client/src/pages/Invoice.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../actions/authActions";
+import { findUserDetails } from "../actions/findUserActions";
+import Invoices from "../components/invoiceForm/index";
+import ResponsiveDrawer from "../components/ResponsiveDrawer/ResponsiveDrawer";
+
+class Invoice extends Component {
+
+    constructor() {
+      super();
+      this.state = {
+        errors: {}
+      };
+    };
+  
+    componentDidMount() {
+      const userData = {
+        id: this.props.auth.user.id
+      };
+      //console.log('componentdid', this.props.auth.user.id)
+      this.props.findUserDetails(userData);
+    }
+  
+    componentWillReceiveProps(nextProps) {
+      if (nextProps.errors) {
+        this.setState({
+          errors: nextProps.errors
+        });
+      }
+    };
+
+    onLogoutClick = e => {
+        e.preventDefault();
+        this.props.logoutUser();
+      };
+
+    render() {
+      const { user } = this.props.auth;
+      const { data } = this.props.userDetails;
+      console.log(user);
+      console.log(data)
+      return (
+        <div >
+            <ResponsiveDrawer />
+            <div>
+                <Invoices clients={{ data }}/>
+            </div> 
+        </div>
+        
+      );
+    }
+  }
+  
+  Invoice.propTypes = {
+    findUserDetails: PropTypes.func.isRequired,
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    userDetails: PropTypes.object.isRequired
+    };
+
+  const mapStateToProps = state => ({
+    auth: state.auth,
+    userDetails: state.findUser.userDetails
+  });
+  
+  export default connect(
+    mapStateToProps,
+    { logoutUser, findUserDetails }
+  )(Invoice);
