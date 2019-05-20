300213b5ece3f844d75bce139e71faa273d7ce24
Kara Nichols <lnicholskara@gmail.com>
2019-05-19 19:30:36 -0700

    working pie chart for project progress

--- client/src/components/DashboardComp/DashboardComp.jsx
+++ client/src/components/DashboardComp/DashboardComp.jsx
@@ -4,19 +4,9 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-// import classnames from 'classnames';
-import Card from '@material-ui/core/Card';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent'; <----- called but never used JRS
-// import Typography from '@material-ui/core/Typography';  <----- called but never used JRS
-// import CardHeader from '@material-ui/core/CardHeader';
-// import CardMedia from '@material-ui/core/CardMedia';
 import '../layout/landingPage/imagesCode/global.css';
 import Highcharts from "highcharts/highstock";
-import HighchartsReact from "highcharts-react-official";
-// import Avatar from '@material-ui/core/Avatar';
-// import IconButton from '@material-ui/core/IconButton';
-// import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 
 const styles = theme => ({
     container: {
@@ -54,46 +44,91 @@ class Dash extends React.Component {
     constructor() {
         super();
         this.state = {
-            errors: {}
-        };
-    }
-
-    beginningState(objectFound, event) {
-        this.setState({ userData: objectFound });
-        console.log(this.state);
-    }
-
-    handleChange = e => {
-        this.setState({
-            userData: Object.assign(
-                {},
-                this.state.userData,
-                { [e.target.id]: e.target.value }
-            ),
-        })
-    }
-    //CARD initial state and event handler
-    state = { expanded: false };
-
-    handleExpandClick = () => {
-        this.setState(state => ({ expanded: !state.expanded }));
+            errors: {},
+        }
     };
 
-    render() {
-        const { classes } = this.props;
+    beginningState(data) {
+        this.setState({ projects: data });
+        console.log(this.state);
+    };
 
-        const { data } = this.props.user;
-        const options = {
-            title: {
-                text: "Recent Activity",
-                align: "left",
+    highChartsReady() {
+        setTimeout(() => {
+            
+        Highcharts.chart('atmospheric-composition', {
+            chart: {
+              type: 'pie',
             },
-            series: [{
-                data: [1, 2, 1, 4, 3, 6, 7, 3, 8, 6, 9]
-            }]
-        };
-        // const bull = <span className={classes.bullet}>•</span>; <----- called but never used ___JRS___
+            title: {
+              verticalAlign: 'middle',
+              floating: true,
+              text: 'Projects',
+              style: {
+                  fontSize: '20px',
+              }
+            },
+            plotOptions: {
+              pie: {
+                  dataLabels: {
+                      format: '{point.name}: {point.percentage:.1f} %'
+                  },
+                innerSize: '60%'
+              }
+            },
+            series: this.state.series
+        });
 
+        },1);
+    };
+
+    projectData() {
+        setTimeout(() => {
+        const projects = this.state.projects;
+        console.log(projects);
+        const count = projects.projects.length;
+        console.log(count);
+        const inactive = Math.round((projects.projects.filter(project => project.status === "inactive").length)/count * 100);
+        const pending =  Math.round((projects.projects.filter(project => project.status === "pending").length)/count * 100);
+        const inprogress = Math.round((projects.projects.filter(project => project.status === "in progress").length)/count * 100);
+        const completed = Math.round((projects.projects.filter(project => project.status === "completed").length)/count * 100);
+        console.log(inactive + " " + pending + " " + inprogress + " " + completed);
+        this.setState({
+            series: [{
+                name: 'Projects',
+                data: [
+                    {
+                    name: 'Inactive',
+                    y: inactive,
+                    color: '#3498db'
+                    },
+                    {
+                    name: 'Pending',
+                    y: pending,
+                    color: '#9b59b6'
+                    },
+                    {
+                    name: 'In Progress',
+                    y: inprogress,
+                    color: '#2ecc71'
+                    },
+                    {
+                    name: 'Completed',
+                    y: completed,
+                    color: '#f1c40f'
+                    }
+                ]
+            }],
+        });
+
+        console.log(this.state)
+        }, 500);
+    }
+
+    render(){
+        //const { classes } = this.props;
+
+        const { data } = this.props.users;
 
         if (!data) {
             console.log(null)
@@ -101,10 +136,11 @@ class Dash extends React.Component {
         }
 
         else {
-            var objectFound = data;
+            console.log(data);
 
-            if (!this.state.userData) {
-                this.beginningState(objectFound);
+            if (!this.state.projects) {
+                this.beginningState(data);
+                this.projectData();
                 return null
             }
             else {
@@ -112,40 +148,35 @@ class Dash extends React.Component {
                 return (
                     <Grid container spacing={24}>
                         <Grid item xs={2} sm={4} md={3} lg={2}>
-                            <Paper className={classes.paper}></Paper>
+                            <Paper></Paper>
                         </Grid>
                         <Grid item xs={9} sm={7} md={8} lg={9}>
                             <h4><strong>Dashboard</strong></h4>
                             <Grid item sm={7} lg={10}>
-                                <HighchartsReact highcharts={
-                                    Highcharts
-                                }
-                                    constructorType={
-                                        "stockChart"
-                                    }
-                                    options={
-                                        options
-                                    }
-                                />
+                                <div id='atmospheric-composition'></div>
+                                {this.highChartsReady()}
                             </Grid>
                         </Grid>
-                    </Grid>
+                    </Grid> 
+                    
                 )
             }
         }
     }
+    
 }
 
 Dash.propTypes = {
     classes: PropTypes.object.isRequired,
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
     auth: state.auth,
     userDetails: state.findUser.userDetails
-});
-
+  });
 
 export default connect(
-    mapStateToProps,
+    mapStateToProps
 )(withStyles(styles)(Dash))
