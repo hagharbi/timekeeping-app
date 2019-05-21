933d6924c775523bb06aa853517b73aab5eecfa5
Santino <ha.agharbi@gmail.com>
2019-05-21 10:41:56 -0700

    switched server to prod env

--- server.js
+++ server.js
@@ -11,12 +11,19 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // API Routes
@@ -27,30 +34,30 @@ app.use("/api/logs", logs);
 
 // Database Setup for Prod Env
 // DB Config
-// const db = require("./config/keys").mongoURI;
+const db = process.env.mongoURI;
 
-// // // Connect to MongoDB
-// mongoose
-//   .connect(
-//     db || "mongodb://localhost/timekeeperapp", {
-//       useNewUrlParser: true
-//     }
-//   )
-//   .then(() => console.log("MongoDB successfully connected"))
-//   .catch(err => console.log(err));
+// // Connect to MongoDB
+mongoose
+  .connect(
+    db || "mongodb://localhost/timekeeperapp", {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 
 // Passport middleware
 app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
-  
+
 // // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/timekeeperapp")
-.then(() => console.log("MongoDB successfully connected"))
-.catch(err => console.log(err));
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> Server listening on PORT ${PORT}!`);
 });
