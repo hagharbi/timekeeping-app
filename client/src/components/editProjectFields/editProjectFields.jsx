9a21e36aca7f72d357ead2404b1287b1e2d2c4d9
Santino <ha.agharbi@gmail.com>
2019-05-20 20:47:57 -0700

    removed inputLabel

--- client/src/components/editProjectFields/editProjectFields.jsx
+++ client/src/components/editProjectFields/editProjectFields.jsx
@@ -15,7 +15,6 @@ import Button from '@material-ui/core/Button';
 
 //Form
 import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
